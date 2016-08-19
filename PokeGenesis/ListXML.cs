c3de436fa688e5df69b85087648a56180c65b4ea
Collin Russell <KakugaGoka@live.com>
2016-08-18 22:55:40 -0700

    First Proto-Build

---
+++ PokeGenesis/ListXML.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GenesisEngine
+{
+    class Identifier
+    {
+        public string id { get; set; }
+    }
+
+    class XMLtoList
+    {
+        public List<Identifier> createList(string xml)
+        {
+            string fileName = (AppDomain.CurrentDomain.BaseDirectory + "XML\\"+xml+".xml");
+            List<Identifier> idList = new List<Identifier>();
+            XDocument doc = XDocument.Load(fileName);
+            var query = from node in doc.Descendants(xml)
+                        select new Identifier
+                        {
+                            id = (string)node.Element("id")
+                        };
+            idList = query.ToList();
+            return idList;
+        }
+    }
+}
