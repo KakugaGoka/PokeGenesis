abc9ef915601af2821bbc34f62f6ae033752be07
Collin Russell <KakugaGoka@live.com>
2016-09-08 16:59:25 -0700

    FIx for build.bat

--- GenesisDex/Build.bat
+++ GenesisDex/Build.bat
@@ -1,3 +1,3 @@
 ﻿@Echo off
 cd C:\Windows\Microsoft.NET\Framework\v4*
-msbuild "%~dp0PokeGenesis.sln" /t:Rebuild /p:Configuration=Debug /p:Platform="Any CPU"
+msbuild "%~dp0..\PokeGenesis.sln" /t:Rebuild /p:Configuration=Debug /p:Platform="Any CPU"
